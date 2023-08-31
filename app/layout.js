6ccf4673d580d400c43ece4d769c7b02e48ffae9
kashifjavaid1 <125742946+kashifjavaid1@users.noreply.github.com>
2023-08-31 05:44:12 -0700

    commit

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
-// import '@bootstrap/dist/css/bootstrap.min.css';
+
 import Navbar from './(components)/navbar/navbar'
-import { Inter } from 'next/font/google'
 import Footer from './(components)/footer/footer';
 export default function RootLayout({ children }) {
   return (
