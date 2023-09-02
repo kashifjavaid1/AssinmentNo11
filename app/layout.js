d8288efd27e968cc05eb309a5488dc28e7df937f
kashifjavaid1 <125742946+kashifjavaid1@users.noreply.github.com>
2023-09-02 10:24:08 -0700

    commit

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Navbar from './(components)/navbar/navbar'
 import Footer from './(components)/footer/footer';
 export default function RootLayout({ children }) {
