83b68d9da58fa936bb358bbcb4385925399bb79e
kashifjavaid1 <125742946+kashifjavaid1@users.noreply.github.com>
2023-09-05 09:43:47 -0700

    commit

--- app/blog/page.js
+++ app/blog/page.js
@@ -4,7 +4,7 @@ function Blog() {
     return(
       <>
        <div className='section1'>
-          <div> 
+        <div> 
             <h1 className='sectionheading'>We're trusted by clients</h1>
           </div>
           <div className='secondcard'>
