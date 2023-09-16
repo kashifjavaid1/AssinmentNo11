b8cb5a082cd5b76fefafcacdf9a76a6930e0e2f8
kashifjavaid1 <125742946+kashifjavaid1@users.noreply.github.com>
2023-09-16 04:37:09 -0700

    commit

--- app/about/page.js
+++ app/about/page.js
@@ -18,7 +18,6 @@ function About() {
          <p className='aboutpera1'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,<br></br>when an unknown printer took a galley of type and scrambled it to make a type specimen book.<br></br> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,<br></br>when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
          <Button btn="Read More"/>
       </div>
-
       </div>
       </>
     );
